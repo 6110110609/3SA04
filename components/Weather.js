596a0280a06293700eab3b96884816e3e4615a42
6110110609 <69430733+6110110609@users.noreply.github.com>
2020-08-20 19:45:08 +0700

    Restore file

--- components/Weather.js
+++ components/Weather.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { Text, ImageBackground, StyleSheet, View } from 'react-native'
+import React, { useState } from 'react'
+import { Text, ImageBackground, StyleSheet } from 'react-native'
 import Forecast from './Forecast'
 
 export default function Weather(props) {
@@ -9,55 +9,20 @@ export default function Weather(props) {
         temp: 0
     })
 
-    useEffect(() => {
-        console.log(`fetching data with zipCode = ${props.zipCode}`)
-        if (props.zipCode) {
-            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=895ddffe33f83710bc1c44e886fe0ec6`)
-                .then((response) => response.json())
-                .then((json) => {
-                    setForecastInfo({
-                        main: json.weather[0].main,
-                        description: json.weather[0].description,
-                        temp: json.main.temp
-                    });
-                })
-                .catch((error) => {
-                    console.warn(error);
-                });
-        }
-    }, [props.zipCode])
-
     return (
         <ImageBackground source={require('../bg.png')} style={styles.backdrop}>
-            <View style={styles.box}>
-                <Text style={styles.text}>Zip Code is {props.zipCode}</Text>
-                {/* <Text>{props.zipCode}</Text> */}
-                <Forecast {...forecastInfo} />
-            </View>
+            <Text>Zip Code</Text>
+            <Text>{props.zipCode}</Text>
+            <Forecast {...forecastInfo} />
         </ImageBackground>
     )
 }
 
 const styles = StyleSheet.create({
     backdrop: {
-        flexDirection: 'row',
-        justifyContent: 'center',
+        flexDirection: 'column',
         alignItems: 'center',
         width: '100%',
         height: '100%'
-    },
-
-    text: {
-        textAlign: 'center',
-        fontSize: 25,
-        color:'white'
-
-    },
-    
-    box: {
-        backgroundColor: 'black',
-        opacity:0.6,
-        padding: 50,
-        width:'100%',
     }
 })
