9612dbfdaed2bb8929b25ef7e257ead1625f519f
unknown <6110110609@psu.ac.th>
2020-08-20 17:45:35 +0700

    challenge

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function Forecast(props) {
     return (
         <View >
-            <Text>{props.main}</Text>
-            <Text>{props.description}</Text>
-            <Text>{props.temp}</Text>
-            <Text>°C</Text>
+            <Text style={styles.text}>{props.main}</Text>
+            <Text style={styles.text}>{props.description}</Text>
+            <Text style={styles.text}>{props.temp} °C</Text>
+            {/* <Text>°C</Text> */}
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        color:'white'
+    }
+})
