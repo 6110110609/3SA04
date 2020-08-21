5850602cc59457aa694a239b79c59cd153dde78e
6110110609 <69430733+6110110609@users.noreply.github.com>
2020-08-21 21:14:11 +0700

    section5

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { FlatList } from 'react-native'
+import { FlatList, View , Text , StyleSheet } from 'react-native'
+import { TouchableHighlight } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
 
 const availableZipItems = [
     { place: 'Hatyai', code: '90110' },
@@ -8,24 +10,38 @@ const availableZipItems = [
     { place: 'Khonkaen', code: '40000' },
     { place: 'Chonburi', code: '20000' },
 ]
-const ZipItem = ({ place, code, navigation }) => (
-    <View>
-        <Text>{place}</Text>
-        <Text>{code}</Text>
-    </View>
+
+const ZipItem = ({place, code, navigation}) => (
+    <TouchableHighlight onPress={() => (
+        navigation.navigate('Weather', {zipCode: code})
+    )}>
+        <View style={styles.zipItem}>
+            <Text>{place}</Text>
+            <Text>{code}</Text>
+        </View>
+    </TouchableHighlight>
 )
-const _keyExtractor = item => item.code
-export default function ZipCodeScreen() {
+export default function zipCodeScreen() {
     const navigation = useNavigation()
     return (
-        <View>
-            <FlatListtList
-                data={availableZipItems}
-                keyExtractor={_keyExtractor}
-                renderItem={({ item }) => <ZipItem {...item} navigation={navigation} />}
-            />
-            <StatusBar style="auto" />
-        </View>
-    );
-
+        <FlatList
+            data = {availableZipItems} 
+            keyExtractor = {item => item.code} 
+            renderItem = {({item}) => <ZipItem {...item} navigation={navigation}/>}
+        />
+    )
 }
+
+const styles = StyleSheet.create({
+    zipItem: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    zipPlace: {
+        flex: 1,
+    },
+    zipCode: {
+        flex: 1,
+    },
+})
